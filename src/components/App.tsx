7f376b4e1d4417132d59c4f78900a3552ac32eab
Andrii Khomenko <akhomenko1995@gmail.com>
2026-02-24 18:59:55 +0100

    test 001

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import Product from './Product.tsx';
+
+export default function App() {
+  return (
+    <>
+      <h1>Best selling</h1>
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?w=640"
+        price={666}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?w=640"
+        price={777}
+      />
+    </>
+  );
+}
