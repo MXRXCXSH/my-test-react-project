56a69b7d27387d8e5f7503d0e598d1430505445e
Andrii Khomenko <akhomenko1995@gmail.com>
2026-02-26 12:31:19 +0100

    test react project part 1

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,9 @@
-import Product from './Product.tsx';
+import UserMenu from './UserMenu';
 
 export default function App() {
   return (
-    <>
-      <h1>Best selling</h1>
-      <Product
-        name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?w=640"
-        price={666}
-      />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?w=640"
-        price={777}
-      />
-    </>
+    <div>
+      <UserMenu name="John Doe" />
+    </div>
   );
 }
---
+++ src/components/book.tsx
@@ -0,0 +1,10 @@
+interface Book {
+  id: string;
+  name: string;
+}
+
+export const books: Book[] = [
+  { id: 'id-1', name: 'JS for beginners' },
+  { id: 'id-2', name: 'React basics' },
+  { id: 'id-3', name: 'React Query overview' },
+];
